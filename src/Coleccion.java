d3d89ff26bfd98006de613aab290bd1b9bc466ba
CarlaHassan <>
2023-10-10 20:55:45 -0300

    modificaciones

---
+++ src/Coleccion.java
@@ -0,0 +1,19 @@
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author CarlaHassan
+ */
+public class Coleccion {
+    
+    private ArrayList<Libro> listaLibros;
+    
+    public Coleccion(){
+        listaLibros = new ArrayList<>();
+    }
+    
+    public boolean agregarLibros(){
+         return listaLibros.add(Libro);
+    }
+}
